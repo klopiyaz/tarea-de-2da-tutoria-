8d24b28cfdd1c7c2ac57a6354d1f286b44c74610
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-07-09 09:38:38 -0500

    Add files via upload

---
+++ 2do triangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2do_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese t: ");
+            double t = double.Parse(Console.ReadLine());
+
+            double aGrados = a * (180.0 / Math.PI);
+
+            double y = t * Math.Sin(a);
+            double z = t * Math.Cos(a);
+            double c = 180 - 90 - a;
+
+            Console.WriteLine("el cateto y es: " + y);
+            Console.WriteLine("el cateto z es: " + z);
+            Console.WriteLine("el angulo c es: " + c);
+        }
+    }
+}
---
+++ taller triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Console.Write("cateto Z: ");
+                //double z = int.Parse(Console.ReadLine());
+                double z = 3;
+
+                //Console.Write("cateto Y: ");
+                //double y = int.Parse(Console.ReadLine());
+                double y = 2;
+
+
+                double t = Math.Sqrt((y * y) + (z * z));
+                double c = Math.Atan(z / y);
+                double a = Math.Atan(y / z);
+
+                double cGrados = c * (180 / Math.PI);
+                double aGrados = a * (180 / Math.PI);
+
+                Console.WriteLine("hipotenusa T es: " + t);
+                Console.WriteLine("angulo c es: " + cGrados);
+                Console.WriteLine("angulo a es: " + aGrados);
+
+            }
+        }
+    }
+}
